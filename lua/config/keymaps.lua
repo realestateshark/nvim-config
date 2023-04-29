b045b70e339d6e2b314084bb6d20a70e00e5f93c
Mario Schillermann <mario.schillermann@gmail.com>
2023-04-29 18:32:50 +0200

    extend custom key mapping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,19 @@
 -- Default options for normal, visual, select and operator modes
 local options = {noremap = true}
 local keymap = vim.api.nvim_set_keymap
+
+-- Normal Mode
+--- Window
+---- Navigation
 keymap("n", "<C-h>", "<C-w>h", options);
 keymap("n", "<C-j>", "<C-w>j", options);
 keymap("n", "<C-k>", "<C-w>k", options);
 keymap("n", "<C-l>", "<C-w>l", options);
+---- Resize
+keymap("n", "<C-Up>", ":resize +10<CR>", options)
+keymap("n", "<C-Down>", ":resize -10<CR>", options)
+keymap("n", "<C-Left>", ":vertical resize -10<CR>", options)
+keymap("n", "<C-Right>", ":vertical resize +10<CR>", options)
+--- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", options)
+keymap("n", "<S-h>", ":bprevious<CR>", options)
