e9bb86e550f2d19c68add8a08003c22941814619
Mario Schillermann <mario.schillermann@gmail.com>
2023-07-28 20:48:18 +0200

    repair keymap config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,45 +7,45 @@ function M.setup()
   require("legendary").setup {
     keymaps = {
       -- Window split
-      { "<C-k>", { n = "<C-w>k" }, description = "Jump to top window", default_opts }, 
-      { "<C-j>", { n = "<C-w>j" }, description = "Jump to bottom window", default_opts }, 
-      { "<C-h>", { n = "<C-w>h" }, description = "Jump to left window", default_opts },
-      { "<C-l>", { n = "<C-w>l" }, description = "Jump to right window", default_opts },
+      { "<C-k>", { n = "<C-w>k" }, description = "Jump to top window", opts = default_opts },
+      { "<C-j>", { n = "<C-w>j" }, description = "Jump to bottom window", opts = default_opts },
+      { "<C-h>", { n = "<C-w>h" }, description = "Jump to left window", opts = default_opts },
+      { "<C-l>", { n = "<C-w>l" }, description = "Jump to right window", opts = default_opts },
       -- Window Split Resize
-      { "<C-Up>", { n = ":resize +10<CR>" }, description = "Increase window height", default_opts },
-      { "<C-Down>", { n = ":resize -10<CR>" }, description = "Decrease window height", default_opts },
-      { "<C-Right>", { n = ":vertical resize +10<CR>" }, description = "Increase window width", default_opts },
-      { "<C-Left>", { n = ":vertical resize -10<CR>" }, description = "Decrease window width", default_opts },
+      { "<C-Up>", { n = ":resize +10<CR>" }, description = "Increase window height", opts = default_opts },
+      { "<C-Down>", { n = ":resize -10<CR>" }, description = "Decrease window height", opts = default_opts },
+      { "<C-Right>", { n = ":vertical resize +10<CR>" }, description = "Increase window width", opts = default_opts },
+      { "<C-Left>", { n = ":vertical resize -10<CR>" }, description = "Decrease window width", opts = default_opts },
       -- Move lines
-      { "<A-k>", { x = ":move '<-2<CR>gv-gv" }, description = "Move lines up", default_opts },
-      { "<A-j>", { x = ":move '>+1<CR>gv-gv" }, description = "Move lines down", default_opts },
+      { "<A-k>", { x = ":move '<-2<CR>gv-gv" }, description = "Move lines up", opts = default_opts },
+      { "<A-j>", { x = ":move '>+1<CR>gv-gv" }, description = "Move lines down", opts = default_opts },
       -- Indent lines
-      { "<", { v = "<gv" }, description = "Indent left", default_opts},
-      { ">", { v = ">gv" }, description = "Indent right", default_opts},
+      { "<", { v = "<gv" }, description = "Indent left", opts = default_opts},
+      { ">", { v = ">gv" }, description = "Indent right", opts = default_opts},
       -- Switch between open files
-      { "<S-l>", { n = ":bnext<CR>" }, description = "Next open file", default_opts },
-      { "<S-h>", { n = ":bprevious<CR>"}, description = "Previous open file", default_opts },
+      { "<S-l>", { n = ":bnext<CR>" }, description = "Next open file", opts = default_opts },
+      { "<S-h>", { n = ":bprevious<CR>"}, description = "Previous open file", opts = default_opts },
       -- LSP diagnostics
-      { "<leader>dp", { n = vim.diagnostic.open_float }, description = "Open diagnostic error popup", default_opts },
-      { "<leader>dl", { n = vim.diagnostic.setloclist }, description = "Open diagnostic error list", default_opts },
-      { "<leader>dk", { n = vim.diagnostic.goto_prev }, description = "Goto previous diagnostic error", default_opts },
-      { "<leader>dj", { n = vim.diagnostic.goto_next }, description = "Goto next diagnostic error", default_opts },
+      { "<leader>dp", { n = vim.diagnostic.open_float }, description = "Open diagnostic error popup", opts = default_opts },
+      { "<leader>dl", { n = vim.diagnostic.setloclist }, description = "Open diagnostic error list", opts = default_opts },
+      { "<leader>dk", { n = vim.diagnostic.goto_prev }, description = "Goto previous diagnostic error", opts = default_opts },
+      { "<leader>dj", { n = vim.diagnostic.goto_next }, description = "Goto next diagnostic error", opts = default_opts },
       -- Explorer
-      { "<C-e>", { n = ":NvimTreeToggle<CR>"}, description = "Open or close explorer", default_opts },
+      { "<C-e>", { n = ":NvimTreeToggle<CR>"}, description = "Open or close explorer", opts = default_opts },
       -- Telescope
-      { "<leader>f", ":Telescope<CR>", description = "open telescope", default_opts },
-      { "<leader>ff", ":Telescope find_files<CR>", description = "Telescope find files", default_opts },
-      { "<leader>fg", ":Telescope live_grep<CR>", description = "Telescope find in files", default_opts },
-      { "<leader>fb", ":Telescope buffers<CR>", description = "Telescope list open files", default_opts },
+      { "<leader>f", ":Telescope<CR>", description = "open telescope", opts = default_opts },
+      { "<leader>ff", ":Telescope find_files<CR>", description = "Telescope find files", opts = default_opts },
+      { "<leader>fg", ":Telescope live_grep<CR>", description = "Telescope find in files", opts = default_opts },
+      { "<leader>fb", ":Telescope buffers<CR>", description = "Telescope list open files", opts = default_opts },
       -- Legendary
-      { "<C-p>", { n = function() require('legendary').find() end }, description = "Legendary command palette", default_opts },
+      { "<C-p>", { n = function() require('legendary').find() end }, description = "Legendary command palette", opts = default_opts },
       -- Terminal
-      { "<leader>tt", { n = function() NTGlobal["terminal"]:toggle() end }, description = "Toggle terminal", default_opts },
-      { "<leader>t1", { n = function() NTGlobal["terminal"]:open(1) end }, description = "Open terminal 1", default_opts },
-      { "<leader>t2", { n = function() NTGlobal["terminal"]:open(2) end }, description = "Open terminal 1", default_opts },
-      { "<leader>t3", { n = function() NTGlobal["terminal"]:open(3) end }, description = "Open terminal 3", default_opts },
-      { "<leader>t4", { n = function() NTGlobal["terminal"]:open(4) end }, description = "Open terminal 4", default_opts },
-      { "<leader>t5", { n = function() NTGlobal["terminal"]:open(5) end }, description = "Open terminal 5", default_opts },
+      { "<leader>tt", { n = function() NTGlobal["terminal"]:toggle() end }, description = "Toggle terminal", opts = default_opts },
+      { "<leader>t1", { n = function() NTGlobal["terminal"]:open(1) end }, description = "Open terminal 1", opts = default_opts },
+      { "<leader>t2", { n = function() NTGlobal["terminal"]:open(2) end }, description = "Open terminal 1", opts = default_opts },
+      { "<leader>t3", { n = function() NTGlobal["terminal"]:open(3) end }, description = "Open terminal 3", opts = default_opts },
+      { "<leader>t4", { n = function() NTGlobal["terminal"]:open(4) end }, description = "Open terminal 4", opts = default_opts },
+      { "<leader>t5", { n = function() NTGlobal["terminal"]:open(5) end }, description = "Open terminal 5", opts = default_opts },
     }
   }
 
