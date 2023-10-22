f5617955ca2e02db58c6d99bee7ae9a2e86cd1ac
Mario Schillermann <mario.schillermann@gmail.com>
2023-10-22 11:49:59 +0200

    install debug plugin for nodejs

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -156,6 +156,62 @@ return packer.startup(function(use)
     end
   }
 
+  use {
+    "rcarriga/nvim-dap-ui"
+    ,
+    requires = {
+      { "mfussenegger/nvim-dap" },
+      { "mxsdev/nvim-dap-vscode-js" },
+      {
+        "microsoft/vscode-js-debug",
+        opt = true,
+        run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out"
+      }
+    },
+    config = function()
+      require("dap-vscode-js").setup({
+        adapters = {
+          'pwa-node',
+          'pwa-chrome',
+          'pwa-msedge',
+          'node-terminal',
+          'pwa-extensionHost'
+        },
+      })
+
+      for _, language in ipairs({ "typescript", "javascript" }) do
+        require("dap").configurations[language] = {
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "pwa-node",
+            request = "attach",
+            name = "Attach",
+            processId = require'dap.utils'.pick_process,
+            cwd = "${workspaceFolder}",
+          }
+        }
+      end
+
+      require("dapui").setup()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  }
+
   if PACKER_BOOTSTRAP then
     require("packer").sync()
   end
