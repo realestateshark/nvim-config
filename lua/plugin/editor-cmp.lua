8e12945c236a4bca30496fa52ff502d1f317f7bd
Mario Schillermann <mario.schillermann@gmail.com>
2023-10-28 20:18:00 +0200

    clean up cmp plugin

--- lua/plugin/editor-cmp.lua
+++ lua/plugin/editor-cmp.lua
@@ -9,15 +9,6 @@ return {
     "saadparwaiz1/cmp_luasnip"
   },
   config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-
-    require("luasnip/loaders/from_vscode").lazy_load()
-    local check_backspace = function()
-      local col = vim.fn.col "." - 1
-      return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
-    end
-
     local kind_icons = {
       Text = "",
       Method = "󰆧",
@@ -46,46 +37,52 @@ return {
       TypeParameter = "󰅲",
     }
 
+    local cmp = require("cmp")
     cmp.setup({
       formatting = {
         format = function(entry, vim_item)
-          vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
-          vim_item.menu = ({
-            buffer = "[Buffer]",
-            nvim_lsp = "[LSP]",
-            nvim_lua = "[Lua]",
-            path = "[Path]",
-            luasnip = "[LuaSnip]",
-          })[entry.source.name]
-          return vim_item
+          local lspkind_ok, lspkind = pcall(require, "lspkind")
+          if not lspkind_ok then
+            -- From kind_icons array
+            vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+            -- Source
+            vim_item.menu = ({
+              buffer = "[Buffer]",
+              nvim_lsp = "[LSP]",
+              luasnip = "[LuaSnip]",
+              nvim_lua = "[Lua]",
+              latex_symbols = "[LaTeX]",
+            })[entry.source.name]
+            return vim_item
+          else
+            -- From lspkind
+            return lspkind.cmp_format()(entry, vim_item)
+          end
         end
       },
       snippet = {
         expand = function(args)
           require('luasnip').lsp_expand(args.body)
-        end
+        end,
       },
       mapping = cmp.mapping.preset.insert({
         ['<C-k>'] = cmp.mapping.scroll_docs(-4),
         ['<C-j>'] = cmp.mapping.scroll_docs(4),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), 
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          elseif luasnip.expandable() then
-            luasnip.expand()
+          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+          -- that way you will only jump inside the snippet region
           elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
-          elseif check_backspace() then
-            fallback()
+          elseif has_words_before() then
+            cmp.complete()
           else
             fallback()
           end
-        end, {
-          "i",
-          "s",
-        }),
+        end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
@@ -94,21 +91,14 @@ return {
           else
             fallback()
           end
-        end, {
-          "i",
-          "s",
-        })
+        end, { "i", "s" }),
       }),
       sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "path" },
-        { name = "luasnip" },
-        { name = "nvim_lua" },
-        { name = "buffer" },
-      }),
-      experimental = {
-        ghost_text = true,
-      }
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+      }, {
+        { name = 'buffer' },
+      })
     })
 
     -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
@@ -118,5 +108,24 @@ return {
         { name = 'buffer' }
       }
     })
+
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        { name = 'cmdline' }
+      })
+    })
+
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    local lspconfig = require('lspconfig')
+    lspconfig.intelephense.setup {
+      capabilities = capabilities
+    }
+    lspconfig.tsserver.setup {
+      capabilities = capabilities
+    }
   end
 }
--- lua/plugin/editor-lsp.lua
+++ lua/plugin/editor-lsp.lua
@@ -17,16 +17,5 @@ return {
       vim.lsp.handlers.signature_help,
       {border = 'rounded'}
     )
-
-    local lspconfig = require("lspconfig")
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-    local capabilities = cmp_nvim_lsp.default_capabilities()
-    lspconfig.intelephense.setup {
-      capabilities = capabilities
-    }
-    lspconfig.tsserver.setup {
-      capabilities = capabilities
-    }
   end
 }
