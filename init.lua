c947b866cacb7bc7075e9e5aa7859a780db77268
Mario Schillermann <mario.schillermann@gmail.com>
2023-10-28 18:18:25 +0200

    create commands to close files

--- init.lua
+++ init.lua
@@ -8,11 +8,21 @@ vim.opt.shiftwidth = 2 -- number of spaces for each indentation
 vim.opt.mouse = "" -- mouse is disabled
 vim.opt.tabstop = 2 -- number of spaces spaces for a tab
 vim.opt.number = true -- turn on line numbering
-vim.opt.relativenumber = true -- turn on relative line numbering
+vim.opt.relativenumber = false -- turn on relative line numbering
 vim.opt.signcolumn = "yes" -- avoid shift text
 vim.opt.linebreak = true -- wrap without split words
 vim.opt.termguicolors = true -- full usage of terminal colors
 vim.opt.splitbelow = true
+
+--
+-- Commands
+--
+vim.api.nvim_create_user_command('CloseOtherFiles', function()
+  vim.cmd("%bdelete|edit #|normal`")
+end,{})
+vim.api.nvim_create_user_command('CloseCurrentFile', function()
+  vim.cmd("bp|bd#")
+end,{})
 
 --
 -- Plugins
