3832996d870938a654ae176181dba449a82d8450
Mario Schillermann <mario.schillermann@gmail.com>
2023-11-02 20:48:41 +0100

    add jest and formatter plugin

--- lua/plugin/commands.lua
+++ lua/plugin/commands.lua
@@ -5,12 +5,3 @@ end, {})
 vim.api.nvim_create_user_command('CloseCurrentFile', function()
   vim.cmd("bp|bd#")
 end, {})
-
--- Format File
-vim.api.nvim_create_user_command('FormatJavaScript', function()
-  vim.cmd("%!npx prettier --stdin-filepath %")
-end, {})
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = {"*.js", "*.ts"},
-  command = "FormatJavaScript",
-})
