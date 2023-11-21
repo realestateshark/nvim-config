e0860e583f30113be4d497cc463981e72dcbd9a8
Mario Schillermann <mario.schillermann@gmail.com>
2023-11-21 07:51:32 +0100

    disable ghost test
    This irritated me while I was writing

--- lua/plugin/lsp-nvim-cmp.lua
+++ lua/plugin/lsp-nvim-cmp.lua
@@ -36,11 +36,6 @@ return {
       TypeParameter = "󰅲",
     }
 
-    local has_words_before = function()
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-
     local cmp = require("cmp")
     cmp.setup({
       formatting = {
@@ -77,8 +72,7 @@ return {
         { name = 'nvim_lsp' },
         { name = 'buffer' },
         { name = "path" },
-      }),
-      experimental = { native_menu = false, ghost_text = { enabled = true } },
+      })
     })
 
     -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
